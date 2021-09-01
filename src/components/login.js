c9de2b644710e3428e2d352285ca4a1033938641
Mudit862 <muditsharma4652@gmail.com>
2021-09-01 23:00:34 +0530

    signup

--- src/components/login.js
+++ src/components/login.js
@@ -16,16 +16,16 @@ function Login(){
                     <input id="password" v-model="password" type="password" placeholder="Your Password"></input>
                 </div>
                 <div className="forgetPassword">
-                <a href="#">Forget Password ?</a>
+                <a href="#">Forget Password?</a>
                 </div>
                 <div className="login">
                     <button className="loginBtn">
-                        LogIn
+                        Log in
                     </button>
                 </div>
                 <div className="signUp">
                     <button className="signupBtn">
-                        SignUp
+                        Sign up
                     </button>
                 </div>
             </div>
