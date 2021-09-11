a7dfce7366fa463f2ce38b0ea87c7c76720b4291
kumar <kumarvermaankit9555@gmail.com>
2021-09-11 15:41:54 +0530

    map

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,21 +1,21 @@
-import React,{useState,useEffect,useRef} from 'react';
-import ReactMapGL, {GeolocateControl,NavigationControl} from "react-map-gl";
+import React, { useState, useEffect, useRef } from 'react';
+import ReactMapGL, { GeolocateControl, NavigationControl } from "react-map-gl";
 // import mapboxgl from 'mapbox-gl'
 import "./map.css"
 import Geocoder from 'react-mapbox-gl-geocoder'
-const geolocateControlStyle= {
+const geolocateControlStyle = {
   right: 10,
   top: 10
 
 };
 
-const navControlStyle= {
-    left: 10,
-    top: 200
-  };
+const navControlStyle = {
+  left: 10,
+  top: 200
+};
 
-  const queryParams = {
-    country: 'in'
+const queryParams = {
+  country: 'in'
 }
 
 
@@ -55,78 +55,78 @@ export default function Map() {
   //   accessToken:process.env.REACT_APP_MAPBOX
   //   });
 
-  
+
 
 
   //   },[]);
 
-// useEffect(()=>{
-//   if(!map.current) return;
+  // useEffect(()=>{
+  //   if(!map.current) return;
 
-//   map.current.on('move', () => {
-//     setLng(map.current.getCenter().lng.toFixed(4));
-//     setLat(map.current.getCenter().lat.toFixed(4));
-//     setZoom(map.current.getZoom().toFixed(2));
-//     });
-
-
-    
+  //   map.current.on('move', () => {
+  //     setLng(map.current.getCenter().lng.toFixed(4));
+  //     setLat(map.current.getCenter().lat.toFixed(4));
+  //     setZoom(map.current.getZoom().toFixed(2));
+  //     });
 
 
-// },[])
 
 
-// useEffect(()=>{
-// map.current.on("load",()=>{
-//   map.current.addControl(new mapboxgl.GeolocateControl({
-//     positionOptions: {
-//     enableHighAccuracy: true
-//     },
-//     trackUserLocation: true,
-//     showUserHeading: true
-//     }));
-// })
 
-// map.current.addControl(nav, 'bottom-right');
-
-// var directions = new mapboxgl.MapboxDirections({
-//   accessToken: process.env.REACT_APP_MAPBOX
-// })
-
-// var t=new mapboxgl.
-// map.addControl(directions, "top-left")
-// },[])
+  // },[])
 
 
-function onselected(viewport,item){
-setViewport(viewport)
-console.log(viewport)
-console.log("item:",item)
-}
+  // useEffect(()=>{
+  // map.current.on("load",()=>{
+  //   map.current.addControl(new mapboxgl.GeolocateControl({
+  //     positionOptions: {
+  //     enableHighAccuracy: true
+  //     },
+  //     trackUserLocation: true,
+  //     showUserHeading: true
+  //     }));
+  // })
+
+  // map.current.addControl(nav, 'bottom-right');
+
+  // var directions = new mapboxgl.MapboxDirections({
+  //   accessToken: process.env.REACT_APP_MAPBOX
+  // })
+
+  // var t=new mapboxgl.
+  // map.addControl(directions, "top-left")
+  // },[])
+
+
+  function onselected(viewport, item) {
+    setViewport(viewport)
+    console.log(viewport)
+    console.log("item:", item)
+  }
 
   return (
     <ReactMapGL {...viewport} width="40vw" height="40vh" onViewportChange={setViewport}
-     mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-     mapStyle="mapbox://styles/darksider51/ckpijb0qi1unb17rtosxrlaz7"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+      mapStyle="mapbox://styles/darksider51/ckpijb0qi1unb17rtosxrlaz7"
     >
 
-                    <Geocoder
-                      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-                      
-                     onSelected={onselected} viewport={viewport} 
-                    queryParams={queryParams}
-                />
+      <Geocoder
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onSelected={onselected}
+        viewport={viewport}
+        queryParams={queryParams}
+      />
       <GeolocateControl
         className="geocontrol"
         style={geolocateControlStyle}
-        positionOptions={{enableHighAccuracy: true}}
+        positionOptions={{ enableHighAccuracy: true }}
         trackUserLocation={true}
         auto
       />
-       <NavigationControl style={navControlStyle} />
+      <NavigationControl style={navControlStyle} />
     </ReactMapGL>
-    
-// {/* <div  ref={mapContainer} className="map-container" /> */}
+
+    // {/* <div  ref={mapContainer} className="map-container" /> */}
 
   );
 }
