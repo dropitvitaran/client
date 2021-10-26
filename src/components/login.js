491865e8a6353e2e788c951f9195989162612d3f
Mudit862 <muditsharma4652@gmail.com>
2021-10-26 10:30:07 +0530

    login-added

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,34 @@
 import React, { useEffect, useState } from "react"
+import { useHistory } from "react-router";
 import "./login.css"
 function Login(){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const history = useHistory();
+    useEffect(() => {
+        if(localStorage.getItem('user-info')){
+            history.push("/home")
+        }
+    },[])
+
+    async function login(){
+        console.warn(email,password);
+        let item = {email,password}
+        let result = await fetch("api_url",{
+            method:'POST',
+            headers:{   
+                "Content-Type":"application/json",
+                "Accept":'applocation/json'
+            },
+            body:JSON.stringify(item)
+        });
+        result= await result.json();
+        localStorage.setItem("user-info", JSON.stringify(result))
+        history.push("/home")
+    }
+
     return(
         <div className="loginPage">
             <div className="loginContainer">
@@ -8,18 +36,18 @@ function Login(){
                 <div>
                     <label className="email">Email</label>
                     <br/>
-                    <input className="input" id="email" v-model="email" type="email" placeholder="Your email address"></input>
+                    <input className="input" id="email" v-model="email" type="email" placeholder="Your email address" onChange = {(e) => setEmail(e.target.value)}></input>
                 </div>
                 <div>
                     <label className="password">Password</label>
                     <br/>
-                    <input className="input" id="password" v-model="password" type="password" placeholder="Your Password"></input>
+                    <input className="input" id="password" v-model="password" type="password" placeholder="Your Password" onChange = {(e) => setPassword(e.target.value)}></input>
                 </div>
                 <div className="forgetPassword">
                 <a href="#">Forget Password?</a>
                 </div>
                 <div className="login">
-                    <button className="loginBtn">
+                    <button className="loginBtn" onClick={login}>
                         Log in
                     </button>
                 </div>
